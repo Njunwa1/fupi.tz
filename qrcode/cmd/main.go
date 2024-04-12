7240aa49c7f5c1c5c4b1c70a6594babede276246
Victor Njunwa <njunwawamavoko@gmail.com>
2024-04-12 19:05:40 +0300

    adds main and tracing functionality

---
+++ qrcode/cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"github.com/Njunwa1/fupi.tz/shortener/config"
+	"github.com/Njunwa1/fupi.tz/shortener/internal/adapters/db"
+	"github.com/Njunwa1/fupi.tz/shortener/internal/adapters/grpc"
+	"github.com/Njunwa1/fupi.tz/shortener/internal/adapters/keygen"
+	"github.com/Njunwa1/fupi.tz/shortener/internal/application/core/api"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"log"
+)
+
+func main() {
+
+	exp, err := exporter(config.GetJaegerURL())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tp, err := tracerProvider(exp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}))
+
+	dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
+	if err != nil {
+		log.Fatalf("Failed to connect to database. Error: %v", err)
+	}
+
+	defer func() {
+		if err = dbAdapter.Client.Disconnect(context.Background()); err != nil {
+			log.Fatalf("Failed to disconnect from database: %s", err)
+		}
+	}()
+
+	keyGenAdapter := keygen.NewAdapter(config.GetKeyGenServiceUrl())
+
+	application := api.NewApplication(dbAdapter, keyGenAdapter)
+	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
+	grpcAdapter.Run()
+}
